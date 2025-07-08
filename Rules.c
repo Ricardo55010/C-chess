a154827b44610b2b982fe3ef20417bd4883fd974
Ricardo55010 <richi_mari.2011@hotmail.com>
2025-07-08 16:00:26 -0600

    Refactor: used variable to make code more adequate

--- Rules.c
+++ Rules.c
@@ -14,15 +14,18 @@ int isOutBounds(int x1,int y1,int x2,int y2){
 }
 
 int isPlayerTurn(Board* board,int x1,int y1){
-    if(board->squares[x1-1][y1-1].piece.player!=board->playerTurn){
-        printf("Invalid move, its not your turn, player %d\n",board->squares[x1-1][y1-1].piece.player);
+    Piece OriginPiece = board->squares[x1-1][y1-1].piece;
+    int currentPlayerTurn = board->playerTurn;
+    if(OriginPiece.player!=currentPlayerTurn){
+        printf("Invalid move, its not your turn, player %d\n",OriginPiece.player);
         return 1;
     }
     return 0;
 }
 
 int isAPiece(Board* board,int x1,int y1){
-    if(board->squares[x1-1][y1-1].piece.type==' '){
+    Piece OriginPiece = board->squares[x1-1][y1-1].piece;
+    if(OriginPiece.type==' '){
         printf("Invalid move, theres no piece in %d %d\n",x1,y1);
         return 1;
     }
