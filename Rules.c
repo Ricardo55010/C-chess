af08b6206ad4dca00b4a4a3f0fc19c23122a09b0
Ricardo55010 <richi_mari.2011@hotmail.com>
2025-07-08 15:56:57 -0600

    Refactor: reduced size of the if conditions with the use of another  variables to point the target and origin piece

--- Rules.c
+++ Rules.c
@@ -32,14 +32,17 @@ int isAPiece(Board* board,int x1,int y1){
 
 
 int isAValidMove(Board* board,int x1,int y1,int x2,int y2){
-    if(board->squares[x1-1][y1-1].piece.type=='P'){
-        if((y1-y2==0 && (x1-x2==2||x1-x2==-2)) && board->squares[x2-1][y2-1].piece.timesMoved==0){
+    Piece OriginPiece = board->squares[x1-1][y1-1].piece;
+    Piece TargetPiece = board->squares[x2-1][y2-1].piece;
+
+    if(OriginPiece.type=='P'){
+        if((y1-y2==0 && (x1-x2==2||x1-x2==-2)) && OriginPiece.timesMoved==0){
             printf("valid move, its a valid move for a pawn that moves for the first time\n");
             printf("x1:%d y1:%d x2:%d y2:%d\n",x1,y1,x2,y2);
             printf("x1-x2:%d y1-y2:%d\n",x1-x2,y1-y2);
             return 0;
         }
-        if(((y1-y2==1||y1-y2==-1) && (x1-x2==1||x1-x2==-1))&& board->squares[x2-1][y2-1].piece.player!=board->squares[x1-1][y1-1].piece.player && board->squares[x2-1][y2-1].piece.type!=' '){
+        if(((y1-y2==1||y1-y2==-1) && (x1-x2==1||x1-x2==-1))&& TargetPiece.player!=OriginPiece.player && TargetPiece.type!=' '){
             printf("Valid move, its a valid move for a pawn that has an enemy piece on the way diagonally\n");
             printf("x1:%d y1:%d x2:%d y2:%d\n",x1,y1,x2,y2);
             printf("x1-x2:%d y1-y2:%d\n",x1-x2,y1-y2);
@@ -53,7 +56,7 @@ int isAValidMove(Board* board,int x1,int y1,int x2,int y2){
             return 1;
         }
     }
-    if(board->squares[x1-1][y1-1].piece.type=='R'){
+    if(OriginPiece.type=='R'){
         if((y1-y2!=0 && x1-x2!=0)){
             printf("Invalid move, its not a valid move for a rook\n");
             printf("x1:%d y1:%d x2:%d y2:%d\n",x1,y1,x2,y2);
@@ -62,7 +65,7 @@ int isAValidMove(Board* board,int x1,int y1,int x2,int y2){
         }
     }
 
-    if(board->squares[x1-1][y1-1].piece.type=='B'){
+    if(OriginPiece.type=='B'){
         if((y1-y2==0 || x1-x2==0)){
             printf("Invalid move, its not a valid move for a bishop\n");
             printf("x1:%d y1:%d x2:%d y2:%d\n",x1,y1,x2,y2);
@@ -71,7 +74,7 @@ int isAValidMove(Board* board,int x1,int y1,int x2,int y2){
         }
     }
 
-    if(board->squares[x1-1][y1-1].piece.type=='N'){
+    if(OriginPiece.type=='N'){
         if(!(((y1-y2==2 || y1-y2==-2) && (x1-x2==1||x1-x2==-1) || (y1-y2==1 || y1-y2==-1) && (x1-x2==2||x1-x2==-2)))){
             printf("Invalid move, its not a valid move for a knight\n");
             printf("x1:%d y1:%d x2:%d y2:%d\n",x1,y1,x2,y2);
@@ -80,7 +83,7 @@ int isAValidMove(Board* board,int x1,int y1,int x2,int y2){
         }
     }
     
-    if(board->squares[x1-1][y1-1].piece.type=='Q'){
+    if(OriginPiece.type=='Q'){
         if((y1-y2==0 || x1-x2==0 || y1-y2!=0 && x1-x2!=0)){
             printf("Invalid move, its not a valid move for a queen\n");
             printf("x1:%d y1:%d x2:%d y2:%d\n",x1,y1,x2,y2);
@@ -90,7 +93,7 @@ int isAValidMove(Board* board,int x1,int y1,int x2,int y2){
     }
 
 
-    if(board->squares[x1-1][y1-1].piece.type=='K'){
+    if(OriginPiece.type=='K'){
         if(((y1-y2>1 || y1-y2<-1) || (x1-x2>1||x1-x2<-1))){
             printf("Invalid move, its not a valid move for a king\n");
             printf("x1:%d y1:%d x2:%d y2:%d\n",x1,y1,x2,y2);
